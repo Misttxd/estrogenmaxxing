84e8c32bfb911bcf52888c0bcd3ba18bae865469
Andreas <andreasbrudovskyy@gmail.com>
2026-07-30 12:01:05 +0200

    done

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -3,14 +3,9 @@ class Solution
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
-        vector<int> v;
 
         for (int i = 0; i < nums.size(); i++){
-            for (int y = 0; y < nums.size(); y++){
-                if (y == i){
-                    continue;
-                }
-
+            for (int y = i + 1; y < nums.size(); y++){
                 int result = nums[i] + nums[y];
 
                 if (result == target){
