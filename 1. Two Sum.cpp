fe501a6a20749cbfaf38621bc08e9e1f5bbeb906
Andreas <andreasbrudovskyy@gmail.com>
2026-07-30 11:58:04 +0200

    done

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -1,11 +1,24 @@
-#include <vector>
-
-using namespace std;
-
 class Solution
 {
 public:
     vector<int> twoSum(vector<int> &nums, int target)
     {
+        vector<int> v;
+
+        for (int i = 0; i < nums.size(); i++){
+            for (int y = 0; y < nums.size(); y++){
+                if (y == i){
+                    continue;
+                }
+
+                int result = nums[i] + nums[y];
+
+                if (result == target){
+                    return {i,y};
+                }
+            }
+        }
+
+        return {};
     }
 };
